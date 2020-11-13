9db928d3e1c08d2279988b831672a36c3e84ff5e
Chris <christian-boer@hotmail.com>
2020-11-13 15:20:06 +0100

    Url update

--- todo-frontend/src/environments/environment.prod.ts
+++ todo-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'cnsd-todo-loadbalancer-911160589.us-east-1.elb.amazonaws.com'
+  apiUrl: 'cnsd-todo-loadbalancer-911160589.us-east-1.elb.amazonaws.com/api/v1'
 };
